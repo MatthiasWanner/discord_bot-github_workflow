40e510712741f107eb8682d491f39ed891bce381
MatthiasWanner <matthias.wanner@gmail.com>
2022-06-16 22:10:51 +0200

    :sparkles: (commands) Add new test slash command to the registered guild

--- src/api/interactions/controllers/postInteraction.controller.ts
+++ src/api/interactions/controllers/postInteraction.controller.ts
@@ -1,17 +1,49 @@
 import { InteractionResponseType, InteractionType } from 'discord-interactions';
-import { Request, Response } from 'express';
+import { RequestHandler } from 'express';
 
-const postInteractionController = (
-  req: Request,
-  res: Response
-): Response<any, Record<string, any>> => {
-  const { type } = req.body;
+import { getRandomEmoji } from '../../../utils';
+
+const postInteractionController: RequestHandler<
+  null,
+  Record<string, any>,
+  {
+    type: number;
+    data: {
+      guild_id: string;
+      id: string;
+      name: string;
+      type: 1;
+    };
+  },
+  null
+> = (req, res) => {
+  const { type, data } = req.body;
 
   /**
    * Handle verification requests
    */
   if (type === InteractionType.PING) {
     return res.send({ type: InteractionResponseType.PONG });
+  }
+
+  /**
+   * Handle slash command requests
+   * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
+   */
+  if (type === InteractionType.APPLICATION_COMMAND) {
+    const { name } = data;
+
+    // "test" guild command
+    if (name === 'test') {
+      // Send a message into the channel where command was triggered from
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          // Fetches a random emoji to send from a helper function
+          content: 'hello world ' + getRandomEmoji(),
+        },
+      });
+    }
   }
 
   return res.send({ type: InteractionResponseType.PONG });

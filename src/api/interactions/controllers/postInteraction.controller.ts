84e159433541951178a2fa285c9e9478aa105ea5
MatthiasWanner <matthias.wanner@gmail.com>
2022-06-17 18:28:10 +0200

    :sparkles: (feat) Create new project role receiving command from channel

--- src/api/interactions/controllers/postInteraction.controller.ts
+++ src/api/interactions/controllers/postInteraction.controller.ts
@@ -1,23 +1,19 @@
 import { InteractionResponseType, InteractionType } from 'discord-interactions';
+import { MessageEmbed } from 'discord.js';
 import { RequestHandler } from 'express';
 
-import { getRandomEmoji } from '../../../utils';
+import { IDiscordChannel } from '../../../types/channels.types';
+import { ICreateRoleBody, IDiscordRole } from '../../../types/roles.types';
+import { DiscordRequest, getRandomEmoji } from '../../../utils';
+import { IInteractionBody } from '../interactions.types';
 
 const postInteractionController: RequestHandler<
   null,
   Record<string, any>,
-  {
-    type: number;
-    data: {
-      guild_id: string;
-      id: string;
-      name: string;
-      type: 1;
-    };
-  },
+  IInteractionBody,
   null
-> = (req, res) => {
-  const { type, data } = req.body;
+> = async (req, res) => {
+  const { type, data, channel_id, guild_id } = req.body;
 
   /**
    * Handle verification requests
@@ -30,7 +26,7 @@ const postInteractionController: RequestHandler<
    * Handle slash command requests
    * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
    */
-  if (type === InteractionType.APPLICATION_COMMAND) {
+  if (type === InteractionType.APPLICATION_COMMAND && data) {
     const { name } = data;
 
     // "test" guild command
@@ -41,6 +37,47 @@ const postInteractionController: RequestHandler<
         data: {
           // Fetches a random emoji to send from a helper function
           content: 'hello world ' + getRandomEmoji(),
+        },
+      });
+    }
+
+    if (name === 'project' && channel_id && guild_id) {
+      // Create the dedicated role
+      const { name = 'channelname' } = await DiscordRequest<IDiscordChannel>(
+        `channels/${channel_id}`,
+        {
+          method: 'GET',
+        }
+      );
+
+      const data: ICreateRoleBody = {
+        mentionable: true,
+        name: `${name}team`,
+      };
+
+      const { id: roleId, name: roleName } = await DiscordRequest<IDiscordRole>(
+        `guilds/${guild_id}/roles`,
+        {
+          method: 'POST',
+          data,
+        }
+      );
+
+      // Send a message into the channel where command was triggered from the server
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          // Fetches a random emoji to send from a helper function
+          content: `New Project linked to this channel 🚀`,
+          embeds: [
+            new MessageEmbed()
+              .setTitle('New team project')
+              .setColor(10181046)
+              .setDescription(
+                `New role ${roleName} was created to tag the team.Add this variable into your github repo secrets`
+              )
+              .addFields({ name: 'DISCORD_ROLE', value: roleId }),
+          ],
         },
       });
     }
